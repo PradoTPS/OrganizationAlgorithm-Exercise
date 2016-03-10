2e61668c3a5992f4750e7f3fab0e7c0979ae60f3
Thiago do Prado Silva <thiagosemjoelho@gmail.com>
2016-03-10 12:25:08 -0300

    Inicio AddRandom
    -Inicio da função que adiciona elementos randomicos na lista;

--- AlgoritimoOrganizacao-Antoanne/AlgoritimoOrganizacao-Antoanne/Algorithm.cs
+++ AlgoritimoOrganizacao-Antoanne/AlgoritimoOrganizacao-Antoanne/Algorithm.cs
@@ -8,6 +8,15 @@ namespace AlgoritimoOrganizacao_Antoanne
 {
     class Algorithm
     {
+        List<int> nbrs = new List<int>();
+        Random random = new Random();
 
+        public void AddRandomElements(int n, List<int> l){
+            for (int i = (l.Count + 1); i <= (l.Count + n); i++)
+            {
+                
+            }
+        }
+            
     }
 }
